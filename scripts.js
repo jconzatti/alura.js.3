0be672bb9a02cbf0cf58247f8d2225009e07a9bf
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-08-27 21:46:15 -0300

    UI

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const lBotaoCarregarImagem = document.getElementById("upload-btn");
+const lEntradaArquivoImagem = document.getElementById("image-upload");
+
+lBotaoCarregarImagem.addEventListener("click", function(){
+    lEntradaArquivoImagem.click();
+})

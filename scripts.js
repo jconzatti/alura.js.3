51d18c7c316ec493ead1a8c55a9e285bae7ce1aa
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-08-29 13:12:33 -0300

    ler arqivo de imagem

--- scripts.js
+++ scripts.js
@@ -3,4 +3,36 @@ const lEntradaArquivoImagem = document.getElementById("image-upload");
 
 lBotaoCarregarImagem.addEventListener("click", function(){
     lEntradaArquivoImagem.click();
+});
+
+function lerConteudoDoArquivo(pArquivo){
+    return new Promise(function(pResolucao, pRejeicao) {
+        const lLeitorDeArquivo = new FileReader();
+        lLeitorDeArquivo.onload = function () {
+            pResolucao({url: lLeitorDeArquivo.result, nome: pArquivo.name})
+        };
+
+        lLeitorDeArquivo.onerror = function () {
+            pRejeicao(`Erro na leitura do arquivo ${pArquivo.name}`)
+        };
+
+        lLeitorDeArquivo.readAsDataURL(pArquivo);
+    });
+}
+
+const lImagemPrincipal = document.querySelector(".main-imagem"); 
+const lNomeDaImagemPrincipal = document.querySelector(".container-imagem-nome p"); 
+
+lEntradaArquivoImagem.addEventListener("change", async function(pEvento){
+    const lArquivoDeImagem = pEvento.target.files[0];
+    
+    if (lArquivoDeImagem){
+        try {
+            const lConteudoDoArquivoDeImagem = await lerConteudoDoArquivo(lArquivoDeImagem);
+            lImagemPrincipal.src = lConteudoDoArquivoDeImagem.url;
+            lNomeDaImagemPrincipal.textContent = lConteudoDoArquivoDeImagem.nome;
+        } catch (lErro) {
+            console.error("Erro na leitura do arquivo");
+        }
+    }
 })
